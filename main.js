cbddcb9ce1485b5edb5b7a77f03cdd1d79f61669
AntonellaMorittu <antonellamorittu@gmail.com>
2024-10-23 12:54:18 +0200

    Create app template using lit-html

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-import './style.css'
+import { html, render } from 'lit-html';
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>
-   Cocktail Assistant
-  </h1>
-`
+const searchCocktails = () => {
+  console.log("Hello world!")
+};
+
+
+const template = () => html`
+  <h1>Cocktail Assistant</h1>
+  <input id="cocktail-input" type="text" placeholder="Search for a cocktail" />
+   <button @click=${searchCocktails}>Search</button>
+
+  <div id="cocktail-results"></div>
+
+  <h2>Shopping List</h2>
+  <ul id="shopping-list"></ul>
+  <button id="print-btn">Print Shopping List</button>
+`;
+
+// Get the container where the template will be rendered
+const appElement = document.querySelector('#app');
+
+// Render the template to the app element
+render(template(), appElement);
